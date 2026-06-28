94424f77e8f76db1b13dfec63baed3a554f3b18a
Core 2 Plus <49093704+Core2Plus-odoo@users.noreply.github.com>
2026-06-28 19:23:46 +0400

    Bump scheduler patch version

--- addons/aabaan_service_scheduler/__manifest__.py
+++ addons/aabaan_service_scheduler/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Aabaan Service Scheduler",
     "summary": "Easy scheduling for pest control and water tank service visits",
-    "version": "19.0.1.0.3",
+    "version": "19.0.1.0.4",
     "category": "Services",
     "author": "Core2Plus",
     "license": "LGPL-3",
